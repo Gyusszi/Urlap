884fcb1e400356641006f6215c59038f1f884258
Vörös Balázs <vorosbalazs0625@gmail.com>
2025-11-25 13:36:50 +0100

    7. cs

--- Urlap/MainWindow.xaml.cs
+++ Urlap/MainWindow.xaml.cs
@@ -21,6 +21,32 @@ namespace Urlap
             InitializeComponent();
         }
 
+        private void btn_rendeles_Click(object sender, RoutedEventArgs e)
+        {
+            string teszta = (lst_teszta.SelectedItem as ListBoxItem)?.Content.ToString() ?? "Nincs kiválasztva";
+
+			string meret = (cmb_meret.SelectedItem as ListBoxItem)?.Content.ToString() ?? "Nincs kiválasztva";
+
+            string feltetek = "";
+            if (cb_sajt.IsChecked == true) feltetek += "Sajt, ";
+			if (cb_sonka.IsChecked == true) feltetek += "Sonka, ";
+			if (cb_gomba.IsChecked == true) feltetek += "Gomba, ";
+			if (cb_szalami.IsChecked == true) feltetek += "Szalámi, ";
+			if (cb_kukorica.IsChecked == true) feltetek += "Kukorica, ";
+			if (cb_parmezan.IsChecked == true) feltetek += "Parmezán, ";
+            if (feltetek.EndsWith(", ")) feltetek = feltetek.Substring(0, feltetek.Length - 2);
+            if (string.IsNullOrEmpty(feltetek)) feltetek = "Nincs feltét";
+
+			string atvetel = rb_Hazhoz.IsChecked == true ? "Házhozszállítás" :
+							 rb_Szemelyes.IsChecked == true ? "Személyes átvétel" :
+							 "nincs kiválasztva";
+
+            txt_osszegzes.Text = $"Rendelés összegzése:\n" +
+							   $"- Tészta: {teszta}\n" +
+							   $"- Méret: {meret}\n" +
+							   $"- Feltétek: {feltetek}\n" +
+							   $"- Átvétel: {atvetel}";
+		}
+	}
 
     }
-}
